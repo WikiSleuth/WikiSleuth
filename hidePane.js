1e551a78f12ed31873e8957dbbb233f56d09a1b4
PatrickDale <patdale216@gmail.com>
2015-01-27 23:16:43 -0600

    Pop webpage back to normal height when UI pane closes

--- hidePane.js
+++ hidePane.js
@@ -1,2 +1,4 @@
 var elem = document.getElementById("panel");
 elem.parentNode.removeChild(elem);
+var wikiPageFooter = document.querySelector("div#footer");
+wikiPageFooter.className = wikiPageFooter.className.replace(' addPaddingForPage', '');
