0955555b9ce2f3c40b4529b5d5f3657c7d7b2e75
Gordon Loery <gloery945@gmail.com>
2015-01-30 15:18:48 -0600

    test case changes

--- UnitTests/unit_test.js
+++ UnitTests/unit_test.js
@@ -1,22 +1,21 @@
 var UnitTester = function() {
 
   this.init = function() {
+  	this.testUrl = "http://en.wikipedia.org/wiki/Cake";
+  	this.WikiRevFinder = new WikiRevFinder(this.testUrl);
     return;
   }
 
   this.firstTest = function(){
-  	var testUrl = "http://en.wikipedia.org/wiki/Cake";
   	var highlightedText = "in magazines";
-
-
-  	WikiAPI = new WikiRevFinder(testUrl);
-  	var revID = WikiAPI.getWikiRevsInfo(highlightedText);
+  	var revID = this.WikiRevFinder.getWikiRevsInfo(highlightedText);
   	if(revID == 624717323){
   		document.body.appendChild(document.createTextNode("Passed test 1."));
   	}
   	else{
   		document.body.appendChild(document.createTextNode("Failed test 1. Expected value = 624717323, actual value = "+revID));
   	}
+  }
   this.hello = function() {
     document.body.appendChild(document.createTextNode('hello'));
     return 'hello';
