31fbe0162f2e3d474f7dd2a41baee4859b079248
laackvet <laackvet@carleton.edu>
2015-02-16 20:29:31 -0600

    fixed landmarks bug when word was bolded

--- getTextAndURL.js
+++ getTextAndURL.js
@@ -1,5 +1,10 @@
 var text = window.getSelection().toString();
-var textTagContent = window.getSelection().anchorNode.parentNode.textContent;
+
+var curNode = window.getSelection().anchorNode;
+while(curNode.parentNode.nodeName != "P"){
+	curNode = curNode.parentNode;
+}
+var textTagContent = curNode.parentNode.textContent;
 //insert '|' by any ending punctuation marks, credit to http://stackoverflow.com/questions/18914629/split-string-into-sentences-in-javascript
 textTagContent = textTagContent.replace(/\.(?!\d)|([^\d])\.(?=\d)/g,'$1.|');
 //create an array of sentences
@@ -7,5 +12,6 @@ var sentences = textTagContent.split('|');
 //grab first and last sentence of paragraph
 var firstSentenceLandmark = sentences[0];
 var endSentenceLandmark = sentences[sentences.length-2];
+
 var url = document.URL;
 [text, url, firstSentenceLandmark, endSentenceLandmark];
