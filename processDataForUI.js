c72a65d28be5e296839df92e2f5ce8e6bb0bd697
PatrickDale <patdale216@gmail.com>
2015-02-20 03:09:05 -0600

    open links in ui in a new tab

--- processDataForUI.js
+++ processDataForUI.js
@@ -20,7 +20,7 @@ function buildHTMLToAdd(tabs, data, callback) {
 			date = timeAry[0] + ":" + timeAry[1] + ", " + dateAry[1] + " " + dateAry[2] + " " + dateAry[3];
 			html += "<li class='rev-list'>" +
 				//<span class='timestamp'>"+date+"</span>
-					    "<label class='selectable_item' for='item" + i + "'> <span class='button'></span> <a href='http://en.wikipedia.org/w/index.php?title="+data[i][0][8]+"&oldid="+data[i][0][5]+"' title="+data[i][0][8]+" class='timestamp'>" + date + "</a>  <span class='editor'>"+data[i][0][1]+" (<a href='http://en.wikipedia.org/wiki/User_talk:"+data[i][0][1]+"' title="+data[i][0][1]+" class='talk'>talk</a> | "+"<a href='http://en.wikipedia.org/wiki/Special:Contributions/"+data[i][0][1]+"' title="+data[i][0][1]+" class='contribs'>contribs</a>) </span>"+
+					    "<label class='selectable_item' for='item" + i + "'> <span class='button'></span> <a href='http://en.wikipedia.org/w/index.php?title="+data[i][0][8]+"&oldid="+data[i][0][5]+"' title="+data[i][0][8]+" target='_blank' class='timestamp'>" + date + "</a>  <span class='editor'>"+data[i][0][1]+" (<a href='http://en.wikipedia.org/wiki/User_talk:"+data[i][0][1]+"' title="+data[i][0][1]+" target='_blank' class='talk'>talk</a> | "+"<a href='http://en.wikipedia.org/wiki/Special:Contributions/"+data[i][0][1]+"' title="+data[i][0][1]+" target='_blank' class='contribs'>contribs</a>) </span>"+
 					    "<span class='comment'>"+data[i][0][2]+"</span></label>" +
 					    "<input type='checkbox' id='item" + i + "' />" +
 					    "<ul class='item_list'>" +
