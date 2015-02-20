fa7c7add759e7b64b690141169d8feac114338fd
laackvet <laackvet@carleton.edu>
2015-02-19 21:30:29 -0600

    trying to get message passing working

--- createPanelForAuthor.js
+++ createPanelForAuthor.js
@@ -1,15 +1,26 @@
 var panelElement = document.createElement('div');
 panelElement.innerHTML = panelHTML;
 document.getElementsByTagName('body')[0].appendChild(panelElement);
+console.log(panelElement);
 var wikiPageFooter = document.querySelector("div#footer");
 wikiPageFooter.className += " addPaddingForPage";
+//element.getElementById("submitbutton").addEventListener("click", saveAuthorName);
 
 
-/*
-var script = document.createElement('script');
-script.setAttribute("type", "application/javascript");
+
+
+/*var script = document.createElement('script');
+script.setAttribute("type", "text/javascript");
 console.log("CREATING PANEL FOR AUTHOR");
-script.textContent = "document.getElementById('submitbutton').addEventListener('click', function() { window.postMessage({type: 'nameOfAuthor', text: 'here'}, '*');}, false);";
+
+script.textContent = "document.getElementById('submitbutton').addEventListener('click', function(){console.log('here');});";
+var codes = document.getElementsByTagName('script')[0].appendChild(panelElement);
+for(var i=0; i<codes.length; i++)
+{
+	eval(codes[i].text);
+}
+
+
 console.log("INSERTING THE SHIT WORKED");
 console.log(script.textContent);
 console.log("$$$$$$$$$$$$$$$$$$$$$");
