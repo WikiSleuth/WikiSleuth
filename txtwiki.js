568f538d54033a22a4be1d16df04101e9c94aba6
Gordon Loery <gloery945@gmail.com>
2015-02-22 17:18:05 -0600

    Revert "fixed bug with txtwiki that was causing errors in cleaning up string"
    This reverts commit 5a90d570ad4172c541dc66b1cd8d06e06bdad8b1.

--- txtwiki.js
+++ txtwiki.js
@@ -43,7 +43,7 @@ var txtwiki = (function(){
     if (content.slice(pos, pos + 2) == "[["){
       var link = "";
       pos += 2;
-      while ((content.slice(pos, pos + 2) != "]]") && (content.slice(pos, pos + 2) != "")){
+      while (content.slice(pos, pos + 2) != "]]"){
         if (content.slice(pos, pos + 2) == "[["){
           var out = parseLink(content, pos);
           link += out.text;
