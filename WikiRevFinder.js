78067d7e8cf51ff7ecd36f64c565713579570c4e
Gordon Loery <gloery945@gmail.com>
2015-02-22 22:51:24 -0600

    made small mistake in disabling caching, fixed now

--- WikiRevFinder.js
+++ WikiRevFinder.js
@@ -35,13 +35,13 @@ var WikiRevFinder = function(url) {
 
 			var midpointRevisionContent = "";
 			// if(this.cachedContent[this.halfpoint] == undefined){
-			// midpointRevisionContent = this.getMidpointRevisionContent();
+			midpointRevisionContent = this.getMidpointRevisionContent();
 				//store the content in the cache for faster retrieval
 				// this.cachedContent[this.halfpoint] = midpointRevisionContent;
 			// }
 			// else{
 				//get the content from the cache instead of recalculating it using the API
-			midpointRevisionContent = this.cachedContent[this.halfpoint];
+			// midpointRevisionContent = this.cachedContent[this.halfpoint];
 			// }
 			var sanitizedMidpointRevisionContent = this.sanitizeInput(midpointRevisionContent);
 			if(sanitizedMidpointRevisionContent.length != 0 && midpointRevisionContent != 0){
